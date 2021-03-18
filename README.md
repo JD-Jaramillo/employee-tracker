9cb5880241b71612076499a4f6884405d57f8080
John Jaramillo <jaramillo784@gmail.com>
2021-03-17 23:31:29 -0400

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # employee-tracker
 Manages a company's employees using node, inquirer, and MySQL.
+
+## Table of Contents
+
+* [Server JS](#JS)
+* [Installation](#Installation)
+* [Contributing](#Contributing)
+
+## JS
+Created the prompts for the user to respond to and add employees, departments, roles etc. Then created queries based on the user prompts to querie mySQL and add or update the database. 
+
+
+Link to Full walkthrough video with narration: 
+![Watch the video]()
+
+
+![JohnDJaramillo-team-profile-generator](./images/rendered-html.png)
+
+
+## Installation
+
+There is no need to install anything, to view the employee tracker simply follow the link below. 
+
+See repository here: [GitHub Repo](https://github.com/JD-Jaramillo/employee-tracker)
+
+## Contributing
+
+If you have any tips or ideas on how to improve the team tracker please reach out to me directly. 
+
+Email: Jaramillo784@gmail.com
--- seeds.sql
+++ seeds.sql
@@ -5,15 +5,33 @@ INSERT INTO department(name)
 VALUES ('HR');
 INSERT INTO department(name)
 VALUES ('Finance');
+INSERT INTO department(name)
+VALUES ('Marketing');
+INSERT INTO department(name)
+VALUES ('Sales');
+INSERT INTO department(name)
+VALUES ('Engineering');
+INSERT INTO department(name)
+VALUES ('Accounting');
 
--- Inserting into role table  
 INSERT INTO role(title, salary, department_id)
-VALUES ('HR Manager', 50, 1);
-INSERT INTO role(title, salary)
-VALUES ('CFO', 95);
+VALUES ('HR Manager', 50000, 1);
+INSERT INTO role(title, salary, department_id)
+VALUES ('Finance Manager', 80000, 1);
+INSERT INTO role(title, salary, department_id)
+VALUES ('Marketing Manager', 75000, 1);
+INSERT INTO role(title, salary, department_id)
+VALUES ('Sales Manager', 90000, 1);
+INSERT INTO role(title, salary, department_id)
+VALUES ('Director of Engineering', 120000, 1);
+INSERT INTO role(title, salary, department_id)
+VALUES ('Comptroller', 110000, 1);
 
--- Inserting into employee table 
-INSERT INTO employee(first_name, last_name)
-VALUES ('Mike', 'Edwards');
-INSERT INTO employee(first_name, last_name)
-VALUES ('Charles', 'Knightly');
+INSERT INTO employee(first_name, last_name, role_id)
+VALUES ('Mike', 'Edwards', 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Stephen', 'Wolf', 3, 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Ryan', 'Priv', 2, 2);
+INSERT INTO employee(first_name, last_name, role_id)
+VALUES ('Mike', 'Edwards', 5);
